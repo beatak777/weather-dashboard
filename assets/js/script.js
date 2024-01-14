b7ecadc938a1f8a664cd77716d95ae226215b61d
Beata Konya <beatakonya@gmail.com>
2024-01-14 23:48:26 +0000

    added api keys and started psuedo coding but taking a break

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-const apiKey = "71b4497f13724d83825077104f9bc741"
+const apiKey = "71b4497f13724d83825077104f9bc741";
+
+const createWeatherCard = (cityName, weatherItem, index)
+// 5 day api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// city name api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+
+// geo code
+//const geoCodeURL = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}';
+
+//reverse geocode 
